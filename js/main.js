58852000b36d730769819e82b51665b8793548dd
thomasweld <thomas.weld@gmail.com>
2016-04-10 15:34:33 -0400

    working on html / css of calculator

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+// // create state object to keep track of the state of the screen + calculations
+//
+// var state = { first: null, second: null, operator: null }
+//
+// // When they press the first number it will change to...
+//
+// var state = { first: 55, second: null, operator: null }
+//
+// // When they hit an operator it will change to...
+//
+// var state = { first: 55, second: null, operator: '+' }
+//
+// // and the third number
+//
+// var state = { first: 55, second: 12, operator: '+' }
